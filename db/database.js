efc729a2e7f995c2c7391adffc74d8250f91d3d2
ArtOleinikov <artem.oleinikov.web@gmail.com>
2025-11-30 07:34:29 +0200

    Refactor database connection and add favorite status update feature
    - Integrated dotenv for environment variable management.
    - Updated database configuration to use PostgreSQL instead of SQLite.
    - Added updateStatusContact function in contactsServices for updating contact favorite status.
    - Introduced updateFavoriteSchema for validating favorite status updates.
    - Enhanced contactsRouter to include a new route for updating contact favorite status.
    - Improved logging message for database connection success.

--- db/database.js
+++ db/database.js
@@ -1,8 +1,11 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./db/database.sqlite",
+const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: "postgres",
   logging: false,
 });
 
